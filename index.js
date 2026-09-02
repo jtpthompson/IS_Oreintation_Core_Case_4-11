bd867db8b3e65b743bd4eccbc0acad947b2ae8e4
jtpthompson <jtpthomp@byu.edu>
2026-09-02 14:41:52 -0600

    Added index.html, added code and two buttons to both index.js and index.html

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-import "./main.css"
+
+// Find the buttons from the HTML
+const careerButton = document.getElementById("careerButton");
+const interviewButton = document.getElementById("interviewButton");
+
+// Career Exploration button
+careerButton.addEventListener("click", function() {
+    window.location.href = "career.html";
+});
+
+// Interview Practice button
+interviewButton.addEventListener("click", function() {
+    window.location.href = "discovery.html";
+});
